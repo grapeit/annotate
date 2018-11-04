e9795bdc2bbf57375c6021e640f68033b5238433
grapeit <alexgrapeit@gmail.com>
2018-11-04 16:34:07 -0600

    Initial Commit

---
+++ annotate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  annotate
+//
+//  Created by Aleksandr Vinogradov on 11/4/18.
+//  Copyright © 2018 grapeit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
